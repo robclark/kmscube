704d8a9314cd29133f77456fc2978cfb5ff3ca8d
Rob Clark <robdclark@gmail.com>
2016-08-30 17:16:55 -0400

    Add YUV and RGB modes
    RGBA uses a single plane RGBA egl image (imported from dma-buf).
    
    NV12_2IMG uses separate Y and UV egl images, and converts to RGB
    explicitly in the fragment shader.
    
    NV12_1IMG uses a single egl image created from two dma-buf's (with
    the conversion to RGB (hypothetically) happening in the driver)

--- kmscube.c
+++ kmscube.c
@@ -24,6 +24,7 @@
 
 /* Based on a egl cube test app originally written by Arvin Schnell */
 
+#include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -35,6 +36,7 @@
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 #include <gbm.h>
+#include <drm/drm_fourcc.h>
 
 #define GL_GLEXT_PROTOTYPES 1
 #include <GLES2/gl2.h>
@@ -47,6 +49,11 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+enum {
+	RGBA,          /* single-plane RGBA */
+	NV12_2IMG,     /* NV12, handled as two textures and converted to RGB in shader */
+	NV12_1IMG,     /* NV12, imported as planar YUV eglimg */
+} mode = RGBA;
 
 static struct {
 	EGLDisplay display;
@@ -54,9 +61,16 @@ static struct {
 	EGLContext context;
 	EGLSurface surface;
 	GLuint program;
+	/* uniform handles: */
 	GLint modelviewmatrix, modelviewprojectionmatrix, normalmatrix;
+	GLint texture, textureuv;
 	GLuint vbo;
-	GLuint positionsoffset, colorsoffset, normalsoffset;
+	GLuint positionsoffset, texcoordsoffset, normalsoffset;
+	GLuint tex[2];
+
+	PFNEGLCREATEIMAGEKHRPROC eglCreateImageKHR;
+	PFNEGLDESTROYIMAGEKHRPROC eglDestroyImageKHR;
+	PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES;
 } gl;
 
 static struct {
@@ -269,37 +283,37 @@ static int init_gl(void)
 			+1.0f, -1.0f, +1.0f,
 	};
 
-	static const GLfloat vColors[] = {
-			// front
-			0.0f,  0.0f,  1.0f, // blue
-			1.0f,  0.0f,  1.0f, // magenta
-			0.0f,  1.0f,  1.0f, // cyan
-			1.0f,  1.0f,  1.0f, // white
-			// back
-			1.0f,  0.0f,  0.0f, // red
-			0.0f,  0.0f,  0.0f, // black
-			1.0f,  1.0f,  0.0f, // yellow
-			0.0f,  1.0f,  0.0f, // green
-			// right
-			1.0f,  0.0f,  1.0f, // magenta
-			1.0f,  0.0f,  0.0f, // red
-			1.0f,  1.0f,  1.0f, // white
-			1.0f,  1.0f,  0.0f, // yellow
-			// left
-			0.0f,  0.0f,  0.0f, // black
-			0.0f,  0.0f,  1.0f, // blue
-			0.0f,  1.0f,  0.0f, // green
-			0.0f,  1.0f,  1.0f, // cyan
-			// top
-			0.0f,  1.0f,  1.0f, // cyan
-			1.0f,  1.0f,  1.0f, // white
-			0.0f,  1.0f,  0.0f, // green
-			1.0f,  1.0f,  0.0f, // yellow
-			// bottom
-			0.0f,  0.0f,  0.0f, // black
-			1.0f,  0.0f,  0.0f, // red
-			0.0f,  0.0f,  1.0f, // blue
-			1.0f,  0.0f,  1.0f  // magenta
+	GLfloat vTexCoords[] = {
+			//front
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			//back
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			//right
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			//left
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			//top
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			//bottom
+			1.0f, 0.0f,
+			0.0f, 0.0f,
+			1.0f, 1.0f,
+			0.0f, 1.0f,
 	};
 
 	static const GLfloat vNormals[] = {
@@ -357,11 +371,12 @@ static int init_gl(void)
 			"                                   \n"
 			"attribute vec4 in_position;        \n"
 			"attribute vec3 in_normal;          \n"
-			"attribute vec4 in_color;           \n"
-			"\n"
+			"attribute vec2 in_TexCoord;        \n"
+			"                                   \n"
 			"vec4 lightSource = vec4(2.0, 2.0, 20.0, 0.0);\n"
 			"                                   \n"
 			"varying vec4 vVaryingColor;        \n"
+			"varying vec2 vTexCoord;            \n"
 			"                                   \n"
 			"void main()                        \n"
 			"{                                  \n"
@@ -371,18 +386,50 @@ static int init_gl(void)
 			"    vec3 vPosition3 = vPosition4.xyz / vPosition4.w;\n"
 			"    vec3 vLightDir = normalize(lightSource.xyz - vPosition3);\n"
 			"    float diff = max(0.0, dot(vEyeNormal, vLightDir));\n"
-			"    vVaryingColor = vec4(diff * in_color.rgb, 1.0);\n"
-			"}                                  \n";
+			"    vVaryingColor = vec4(diff * vec3(1.0, 1.0, 1.0), 1.0);\n"
+			"    vTexCoord = in_TexCoord; \n"
+			"}                            \n";
 
-	static const char *fragment_shader_source =
+	static const char *fragment_shader_source_1img =
+			"#extension GL_OES_EGL_image_external : enable\n"
 			"precision mediump float;           \n"
 			"                                   \n"
+			"uniform samplerExternalOES uTex;   \n"
+			"                                   \n"
 			"varying vec4 vVaryingColor;        \n"
+			"varying vec2 vTexCoord;            \n"
 			"                                   \n"
 			"void main()                        \n"
 			"{                                  \n"
-			"    gl_FragColor = vVaryingColor;  \n"
+			"    gl_FragColor = vVaryingColor * texture2D(uTex, vTexCoord);\n"
 			"}                                  \n";
+
+	static const char *fragment_shader_source_2img =
+			"#extension GL_OES_EGL_image_external : enable  \n"
+			"precision mediump float;                       \n"
+			"                                               \n"
+			"uniform samplerExternalOES uTexY;              \n"
+			"uniform samplerExternalOES uTexUV;             \n"
+			"                                               \n"
+			"varying vec4 vVaryingColor;                    \n"
+			"varying vec2 vTexCoord;                        \n"
+			"                                               \n"
+			"mat4 csc = mat4(1.0,  0.0,    1.402, -0.701,   \n"
+			"                1.0, -0.344, -0.714,  0.529,   \n"
+			"                1.0,  1.772,  0.0,   -0.886,   \n"
+			"                0.0,  0.0,    0.0,    0.0);    \n"
+			"                                               \n"
+			"void main()                                    \n"
+			"{                                              \n"
+			"    vec4 yuv;                                  \n"
+			"    yuv.x  = texture2D(uTexY,  vTexCoord).x;   \n"
+			"    yuv.yz = texture2D(uTexUV, vTexCoord).xy;  \n"
+			"    yuv.w  = 1.0;                              \n"
+			"    gl_FragColor = vVaryingColor * (yuv * csc);\n"
+			"}                                              \n";
+
+	const char *fragment_shader_source = (mode == NV12_2IMG) ?
+			fragment_shader_source_2img : fragment_shader_source_1img;
 
 	gl.display = eglGetDisplay(gbm.dev);
 
@@ -390,6 +437,16 @@ static int init_gl(void)
 		printf("failed to initialize\n");
 		return -1;
 	}
+
+
+#define get_proc(name) do { \
+		gl.name = (void *)eglGetProcAddress(#name); \
+		assert(gl.name); \
+	} while (0)
+
+	get_proc(eglCreateImageKHR);
+	get_proc(eglDestroyImageKHR);
+	get_proc(glEGLImageTargetTexture2DOES);
 
 	printf("Using display %p with EGL version %d.%d\n",
 			gl.display, major, minor);
@@ -499,27 +556,269 @@ static int init_gl(void)
 	gl.modelviewmatrix = glGetUniformLocation(gl.program, "modelviewMatrix");
 	gl.modelviewprojectionmatrix = glGetUniformLocation(gl.program, "modelviewprojectionMatrix");
 	gl.normalmatrix = glGetUniformLocation(gl.program, "normalMatrix");
+	if (mode == NV12_2IMG) {
+		gl.texture   = glGetUniformLocation(gl.program, "uTexY");
+		gl.textureuv = glGetUniformLocation(gl.program, "uTexUV");
+	} else {
+		gl.texture   = glGetUniformLocation(gl.program, "uTex");
+	}
 
 	glViewport(0, 0, drm.mode->hdisplay, drm.mode->vdisplay);
 	glEnable(GL_CULL_FACE);
 
 	gl.positionsoffset = 0;
-	gl.colorsoffset = sizeof(vVertices);
-	gl.normalsoffset = sizeof(vVertices) + sizeof(vColors);
+	gl.texcoordsoffset = sizeof(vVertices);
+	gl.normalsoffset = sizeof(vVertices) + sizeof(vTexCoords);
+
 	glGenBuffers(1, &gl.vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, gl.vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices) + sizeof(vColors) + sizeof(vNormals), 0, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices) + sizeof(vTexCoords) + sizeof(vNormals), 0, GL_STATIC_DRAW);
 	glBufferSubData(GL_ARRAY_BUFFER, gl.positionsoffset, sizeof(vVertices), &vVertices[0]);
-	glBufferSubData(GL_ARRAY_BUFFER, gl.colorsoffset, sizeof(vColors), &vColors[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, gl.texcoordsoffset, sizeof(vTexCoords), &vTexCoords[0]);
 	glBufferSubData(GL_ARRAY_BUFFER, gl.normalsoffset, sizeof(vNormals), &vNormals[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (const GLvoid *)(intptr_t)gl.positionsoffset);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (const GLvoid *)(intptr_t)gl.normalsoffset);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (const GLvoid *)(intptr_t)gl.colorsoffset);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (const GLvoid *)(intptr_t)gl.texcoordsoffset);
 	glEnableVertexAttribArray(2);
 
 	return 0;
+}
+
+static const uint32_t texw = 512, texh = 512;
+
+static int get_fd_rgba(uint32_t *pstride)
+{
+	struct gbm_bo *bo;
+	void *map_data = NULL;
+	uint32_t stride;
+	extern const uint32_t raw_512x512_rgba[];
+	uint8_t *map, *src = (uint8_t *)raw_512x512_rgba;
+	int fd;
+
+	/* NOTE: do not actually use GBM_BO_USE_WRITE since that gets us a dumb buffer: */
+	bo = gbm_bo_create(gbm.dev, texw, texh, GBM_FORMAT_ARGB8888, GBM_BO_USE_LINEAR);
+
+	map = gbm_bo_map(bo, 0, 0, texw, texh, GBM_BO_TRANSFER_WRITE, &stride, &map_data);
+
+	for (int i = 0; i < texh; i++) {
+		memcpy(&map[stride * i], &src[texw * 4 * i], texw * 4);
+	}
+
+	gbm_bo_unmap(bo, map_data);
+
+	fd = gbm_bo_get_fd(bo);
+
+	/* we have the fd now, no longer need the bo: */
+	gbm_bo_destroy(bo);
+
+	*pstride = stride;
+
+	return fd;
+}
+
+static int get_fd_y(uint32_t *pstride)
+{
+	struct gbm_bo *bo;
+	void *map_data = NULL;
+	uint32_t stride;
+	extern const uint32_t raw_512x512_nv12[];
+	uint8_t *map, *src = (uint8_t *)raw_512x512_nv12;
+	int fd;
+
+	/* NOTE: do not actually use GBM_BO_USE_WRITE since that gets us a dumb buffer: */
+	/* hmm, no R8/R8G8 gbm formats?? */
+	bo = gbm_bo_create(gbm.dev, texw/4, texh, GBM_FORMAT_ARGB8888, GBM_BO_USE_LINEAR);
+
+	map = gbm_bo_map(bo, 0, 0, texw/4, texh, GBM_BO_TRANSFER_WRITE, &stride, &map_data);
+
+	for (int i = 0; i < texh; i++) {
+		memcpy(&map[stride * i], &src[texw * i], texw);
+	}
+
+	gbm_bo_unmap(bo, map_data);
+
+	fd = gbm_bo_get_fd(bo);
+
+	/* we have the fd now, no longer need the bo: */
+	gbm_bo_destroy(bo);
+
+	*pstride = stride;
+
+	return fd;
+}
+
+static int get_fd_uv(uint32_t *pstride)
+{
+	struct gbm_bo *bo;
+	void *map_data = NULL;
+	uint32_t stride;
+	extern const uint32_t raw_512x512_nv12[];
+	uint8_t *map, *src = &((uint8_t *)raw_512x512_nv12)[texw * texh];
+	int fd;
+
+	/* NOTE: do not actually use GBM_BO_USE_WRITE since that gets us a dumb buffer: */
+	/* hmm, no R8/R8G8 gbm formats?? */
+	bo = gbm_bo_create(gbm.dev, texw/2/2, texh/2, GBM_FORMAT_ARGB8888, GBM_BO_USE_LINEAR);
+
+	map = gbm_bo_map(bo, 0, 0, texw/2/2, texh/2, GBM_BO_TRANSFER_WRITE, &stride, &map_data);
+
+	for (int i = 0; i < texh/2; i++) {
+		memcpy(&map[stride * i], &src[texw * i], texw);
+	}
+
+	gbm_bo_unmap(bo, map_data);
+
+	fd = gbm_bo_get_fd(bo);
+
+	/* we have the fd now, no longer need the bo: */
+	gbm_bo_destroy(bo);
+
+	*pstride = stride;
+
+	return fd;
+}
+
+static int init_tex_rgba(void)
+{
+	uint32_t stride;
+	int fd = get_fd_rgba(&stride);
+	const EGLint attr[] = {
+		EGL_WIDTH, texw,
+		EGL_HEIGHT, texh,
+		EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_ABGR8888,
+		EGL_DMA_BUF_PLANE0_FD_EXT, fd,
+		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE0_PITCH_EXT, stride,
+		EGL_NONE
+	};
+	EGLImage img;
+
+	glGenTextures(1, gl.tex);
+
+	img = gl.eglCreateImageKHR(gl.display, EGL_NO_CONTEXT,
+			EGL_LINUX_DMA_BUF_EXT, NULL, attr);
+	assert(img);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_EXTERNAL_OES, gl.tex[0]);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	gl.glEGLImageTargetTexture2DOES(GL_TEXTURE_EXTERNAL_OES, img);
+
+	gl.eglDestroyImageKHR(gl.display, img);
+
+	return 0;
+}
+
+static int init_tex_nv12_2img(void)
+{
+	uint32_t stride_y, stride_uv;
+	int fd_y = get_fd_y(&stride_y);
+	int fd_uv = get_fd_uv(&stride_uv);
+	const EGLint attr_y[] = {
+		EGL_WIDTH, texw,
+		EGL_HEIGHT, texh,
+		EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_R8,
+		EGL_DMA_BUF_PLANE0_FD_EXT, fd_y,
+		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE0_PITCH_EXT, stride_y,
+		EGL_NONE
+	};
+	const EGLint attr_uv[] = {
+		EGL_WIDTH, texw/2,
+		EGL_HEIGHT, texh/2,
+		EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_GR88,
+		EGL_DMA_BUF_PLANE0_FD_EXT, fd_uv,
+		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE0_PITCH_EXT, stride_uv,
+		EGL_NONE
+	};
+	EGLImage img_y, img_uv;
+
+	glGenTextures(2, gl.tex);
+
+	/* Y plane texture: */
+	img_y = gl.eglCreateImageKHR(gl.display, EGL_NO_CONTEXT,
+			EGL_LINUX_DMA_BUF_EXT, NULL, attr_y);
+	assert(img_y);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_EXTERNAL_OES, gl.tex[0]);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	gl.glEGLImageTargetTexture2DOES(GL_TEXTURE_EXTERNAL_OES, img_y);
+
+	gl.eglDestroyImageKHR(gl.display, img_y);
+
+	/* UV plane texture: */
+	img_uv = gl.eglCreateImageKHR(gl.display, EGL_NO_CONTEXT,
+			EGL_LINUX_DMA_BUF_EXT, NULL, attr_uv);
+	assert(img_uv);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_EXTERNAL_OES, gl.tex[1]);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	gl.glEGLImageTargetTexture2DOES(GL_TEXTURE_EXTERNAL_OES, img_uv);
+
+	gl.eglDestroyImageKHR(gl.display, img_uv);
+
+	return 0;
+}
+
+static int init_tex_nv12_1img(void)
+{
+	uint32_t stride_y, stride_uv;
+	int fd_y = get_fd_y(&stride_y);
+	int fd_uv = get_fd_uv(&stride_uv);
+	const EGLint attr[] = {
+		EGL_WIDTH, texw,
+		EGL_HEIGHT, texh,
+		EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_NV12,
+		EGL_DMA_BUF_PLANE0_FD_EXT, fd_y,
+		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE0_PITCH_EXT, stride_y,
+		EGL_DMA_BUF_PLANE1_FD_EXT, fd_uv,
+		EGL_DMA_BUF_PLANE1_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE1_PITCH_EXT, stride_uv,
+		EGL_NONE
+	};
+	EGLImage img;
+
+	glGenTextures(1, gl.tex);
+
+	img = gl.eglCreateImageKHR(gl.display, EGL_NO_CONTEXT,
+			EGL_LINUX_DMA_BUF_EXT, NULL, attr);
+	assert(img);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_EXTERNAL_OES, gl.tex[0]);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	gl.glEGLImageTargetTexture2DOES(GL_TEXTURE_EXTERNAL_OES, img);
+
+	gl.eglDestroyImageKHR(gl.display, img);
+
+	return 0;
+}
+
+static int init_tex(void)
+{
+	switch (mode) {
+	case RGBA:
+		return init_tex_rgba();
+	case NV12_2IMG:
+		return init_tex_nv12_2img();
+	case NV12_1IMG:
+		return init_tex_nv12_1img();
+	}
+	return -1;
 }
 
 static void draw(uint32_t i)
@@ -560,6 +859,10 @@ static void draw(uint32_t i)
 	glUniformMatrix4fv(gl.modelviewmatrix, 1, GL_FALSE, &modelview.m[0][0]);
 	glUniformMatrix4fv(gl.modelviewprojectionmatrix, 1, GL_FALSE, &modelviewprojection.m[0][0]);
 	glUniformMatrix3fv(gl.normalmatrix, 1, GL_FALSE, normal);
+	glUniform1i(gl.texture, 0); /* '0' refers to texture unit 0. */
+
+	if (mode == NV12_2IMG)
+		glUniform1i(gl.textureuv, 1);
 
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	glDrawArrays(GL_TRIANGLE_STRIP, 4, 4);
@@ -629,6 +932,16 @@ int main(int argc, char *argv[])
 	uint32_t i = 0;
 	int ret;
 
+	if (argc > 1) {
+		if (strcmp(argv[1], "RGBA") == 0) {
+			mode = RGBA;
+		} else if (strcmp(argv[1], "NV12_2IMG") == 0) {
+			mode = NV12_2IMG;
+		} else if (strcmp(argv[1], "NV12_1IMG") == 0) {
+			mode = NV12_1IMG;
+		}
+	}
+
 	ret = init_drm();
 	if (ret) {
 		printf("failed to initialize DRM\n");
@@ -648,6 +961,12 @@ int main(int argc, char *argv[])
 	ret = init_gl();
 	if (ret) {
 		printf("failed to initialize EGL\n");
+		return ret;
+	}
+
+	ret = init_tex();
+	if (ret) {
+		printf("failed to initialize EGLImage texture\n");
 		return ret;
 	}
 
