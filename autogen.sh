ea39f2976835c795a39dbfb2f8f1102e468e4af4
Emil Velikov <emil.l.velikov@gmail.com>
2017-03-13 14:02:46 +0000

    autogen.sh: set default sendemail.to/format.subjectPrefix
    Provide some sane defaults for patch submission.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,12 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd "$srcdir"
 
+git config --local --get format.subjectPrefix >/dev/null ||
+    git config --local format.subjectPrefix "PATCH kmscube" 2>/dev/null
+
+git config --local --get sendemail.to >/dev/null ||
+    git config --local sendemail.to "mesa-dev@lists.freedesktop.org" 2>/dev/null
+
 autoreconf --force --verbose --install || exit 1
 cd "$ORIGDIR" || exit $?
 
