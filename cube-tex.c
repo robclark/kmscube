1b22f28ddf23a77c82ff80f73252e0cb3176e68c
Fabio Estevam <festevam@gmail.com>
2017-03-15 18:56:53 -0300

    cube-tex: Handle SMOOTH switch case
    In kmscube.c there is the following logic:
    
    	if (mode == SMOOTH) {
    		egl = init_cube_smooth(gbm);
    	} else {
    		egl = init_cube_tex(gbm, mode);
    	}
    
    ,which makes init_cube_tex() to be never called on the SMOOTH case.
    
    Handle the SMOOTH switch case inside init_tex() to fix the following
    build warning:
    
    cube-tex.c: In function 'init_tex':
    cube-tex.c:438:2: warning: enumeration value 'SMOOTH' not handled in switch [-Wswitch]
      switch (mode) {
      ^
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Eric Engestrom <eric@engestrom.ch>
    [Eric: add assert()]
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>

--- cube-tex.c
+++ cube-tex.c
@@ -442,6 +442,9 @@ static int init_tex(enum mode mode)
 		return init_tex_nv12_2img();
 	case NV12_1IMG:
 		return init_tex_nv12_1img();
+	case SMOOTH:
+		assert(!"unreachable");
+		return -1;
 	}
 	return -1;
 }
